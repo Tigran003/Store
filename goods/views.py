f529bbbd9153eb3bc5753842493fec906b64c5b4
tigran <sergeevt003@gmail.com>
2024-05-15 16:58:48 +0400

    'added search'

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,22 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
-from goods.models import Products
+from  goods.models import Products
+from  goods.utils import  search_q
 
 
-def catalog(request, category_slug) -> HttpResponse:
+def catalog(request, category_slug=None) -> HttpResponse:
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all-products':
         goods = Products.objects.all()
+
+    elif query:
+        goods = search_q(query)
+
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
