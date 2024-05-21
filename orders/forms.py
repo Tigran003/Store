12825eb7128cf52bdb47b616fd245b0f49c535da
tigran <sergeevt003@gmail.com>
2024-05-21 21:48:01 +0400

    'Custom validators'

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,10 @@
+import re
+
 from django import forms
 
 
 class CreateOrderForm(forms.Form):
+
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
@@ -16,3 +19,16 @@ class CreateOrderForm(forms.Form):
         ('0','False'),
         ('1','True'),
     ],)
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Phone number must contain only numbers")
+
+        pattern = re.compile(r'^\d{12}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Not a valid phone number format")
+
+        return data
+
